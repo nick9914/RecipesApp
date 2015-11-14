5d5c7c586e244f222ce68f9e33b0d4e28ac9f654
ztang <nicktang1992@163.com>
2015-11-14 15:58:14 -0500

    Zhengyang Tang test commit

--- app/src/main/java/course/examples/recipewizard/MainActivity.java
+++ app/src/main/java/course/examples/recipewizard/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*TEST1*/
+        /*some change*/
         ArrayList<Integer> test = new ArrayList<>();
     }
 
