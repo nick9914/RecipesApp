132a0bdf49bcdfaa52f09a40cf16a5ca67f4fc8f
Andrew-J-Cleary <Andrew_J_Cleary@yahoo.com>
2015-11-15 01:56:27 -0500

    named activities

--- app/src/main/java/course/examples/recipewizard/MainActivity.java
+++ app/src/main/java/course/examples/recipewizard/MainActivity.java
@@ -1,9 +1,11 @@
 package course.examples.recipewizard;
 //  Authors : Andrew Cleary,
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.ArrayList;
@@ -17,6 +19,53 @@ public class MainActivity extends Activity {
         /*TEST1*/
         /*some change */
         ArrayList<Integer> test = new ArrayList<>();
+
+        // Get a reference to the Press Me Button
+        final Button button = (Button) findViewById(R.id.button);
+        // Set an OnClickListener on this Button
+        // Called each time the user clicks the Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // start activity: Find Recipes
+                Intent intent = new Intent(MainActivity.this, RecipesActivity.class);
+                startActivity(intent);
+
+
+
+            }
+        });
+        // Get a reference to the Press Me Button
+        final Button button2 = (Button) findViewById(R.id.button2);
+        // Set an OnClickListener on this Button
+        // Called each time the user clicks the Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // start activity: Add New Recipe
+                Intent intent = new Intent(MainActivity.this, AddRecipeActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
+        // Get a reference to the Press Me Button
+        final Button button3 = (Button) findViewById(R.id.button3);
+        // Set an OnClickListener on this Button
+        // Called each time the user clicks the Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // start activity: Add Ingredients
+                Intent intent = new Intent(MainActivity.this, IngredientsActivity.class);
+                startActivity(intent);
+
+
+            }
+        });
     }
 
     @Override
