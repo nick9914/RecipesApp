3677bf81e6df7f8851191e31f2d66d87aeafcad1
Andrew-J-Cleary <Andrew_J_Cleary@yahoo.com>
2015-11-15 02:28:39 -0500

    named activities

--- app/src/main/java/course/examples/recipewizard/FilterActivity.java
+++ app/src/main/java/course/examples/recipewizard/FilterActivity.java
@@ -1,7 +1,63 @@
 package course.examples.recipewizard;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
 /**
  * Created by Andrew on 11/15/2015.
  */
-public class FilterActivity {
+public class FilterActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+
+
+        // Get a reference to the Press Me Button
+        final Button button = (Button) findViewById(R.id.button);
+        // Set an OnClickListener on this Button
+        // Called each time the user clicks the Button
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                // return selections based on database fields
+                //intent.putExtra("field","value");
+                finish();
+
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
