c6abbeba511e0b4035ab274996f33cec05dd2324
Andrew-J-Cleary <Andrew_J_Cleary@yahoo.com>
2015-12-06 00:49:59 -0500

    disable excludeing ingredients

--- app/src/main/java/course/examples/recipewizard/RecipesActivity.java
+++ app/src/main/java/course/examples/recipewizard/RecipesActivity.java
@@ -227,7 +227,9 @@ public class RecipesActivity extends Activity {
                 /*Construct GET URL*/
                 StringBuilder urlWithParameters = new StringBuilder(BASE_URL);
 
-                urlWithParameters.append(buildURLExcludes(mIngredientListProvided, mIngredientList));
+                //TODO :
+                //comment in this line to enable the exclude file
+                //urlWithParameters.append(buildURLExcludes(mIngredientListProvided, mIngredientList));
                 urlWithParameters.append(buildURLIncludes(mIngredientListProvided, mIngredientListIncludes));
                 urlWithParameters.append("&start=" + paginationFrom);
                 urlWithParameters.append("&maxResult=" + MAX_RESULT);
